2498d2714648d104b3ecf13d98149dc0f21f8fe9
Hamza <realvirtualunity@gmail.com>
2025-05-19 19:56:19 -0400

    pushing new sever

--- app.js
+++ app.js
@@ -1,16 +1,42 @@
-const express = require('express');
-const path = require('path');
-const indexRouter = require('./routes/index');
+var bodyParser = require('body-parser');
+var express = require('express');
+var app = express();
+var xhub = require('express-x-hub');
 
-const app = express();
-const PORT = 3000;
+app.set('port', (process.env.PORT || 3000));
+app.listen(app.get('port'));
 
-// Serve static files from the "public" directory
-app.use(express.static(path.join(__dirname, 'public')));
+const appSecret = '1234567890';
 
-// Use the router for handling routes
-app.use('/', indexRouter);
+app.use(xhub({ algorithm: 'sha1', secret: appSecret }));
+app.use(bodyParser.json());
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}/`);
+var token = process.env.TOKEN || 'token';
+var received_updates = [];
+
+app.get('/', function(req, res) {
+  console.log(req);
+  res.send('<pre>' + JSON.stringify(received_updates, null, 2) + '</pre>');
 });
+
+app.get(['/facebook', '/instagram', '/threads'], function(req, res) {
+  if (
+    req.query['hub.mode'] == 'subscribe' &&
+    req.query['hub.verify_token'] == token
+  ) {
+    res.send(req.query['hub.challenge']);
+  } else {
+    res.sendStatus(400);
+  }
+});
+
+app.post('/instagram', function(req, res) {
+  console.log('Instagram request body:');
+  console.log(req.body);
+  // Process the Instagram updates here
+  received_updates.unshift(req.body);
+  res.sendStatus(200);
+});
+
+
+app.listen();
